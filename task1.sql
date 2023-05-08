990412afe192129a772d002cb922f9947ede0b93
TechAndSam <kanmbiey@gmail.com>
2023-05-08 13:23:41 +0100

    week 2 task added

---
+++ task1.sql
@@ -0,0 +1,9 @@
+ SELECT Customers.FullName, Bookings.BookingID 
+ FROM Customers INNER JOIN Bookings 
+ ON Customers.CustomerID = Bookings.CustomerID;
+ 
+ SELECT Customers.FullName, Bookings.BookingID 
+ FROM Customers INNER JOIN Bookings 
+ ON Customers.CustomerID = Bookings.CustomerID;
+ 
+ 
